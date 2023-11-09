1a3b08c487fd1369a99515e43f70039e7209947d
Jessica Nelson <jessica.nelson60@gmail.com>
2023-11-09 16:31:52 -0700

    added object array

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  let newItem = '';
+
+let todoList = [
+  {
+    text: 'Buy groceries', 
+    status: true
+  },
+  {
+    text: 'Complete coding assignment', 
+    status: true
+  },
+  {
+    text: 'Update resume and LinkedIn profile', 
+    status: true
+  },
+  {
+    text: 'Go for a 30 minute walk', 
+    status: false
+  },
+  {
+    text: 'Learn a new programming language feature', 
+    status: false
+  },
+  {
+    text: 'Try a new recipe for dinner', 
+    status: false
+  },
+];
+
